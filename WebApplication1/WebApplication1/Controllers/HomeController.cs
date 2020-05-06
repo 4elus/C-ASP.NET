d5454d597b8286b6ffe4e76fe368accabf8d02bf
4elus <m_alex02@mail.ru>
2020-05-06 17:44:37 +0400

    Добавлена возможность добавления и редактирования данных

--- WebApplication1/WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+
 namespace WebApplication1.Controllers
 {
     public class HomeController : Controller
@@ -11,16 +12,13 @@ namespace WebApplication1.Controllers
         public ActionResult Index()
         {
             Model1 db = new Model1();
-            var films = (from film in db.FILMS select film).ToList();
-
-
+            var films = (from film in db.FILMS select film).Take(9).ToList();
             return View(films);
         }
 
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
@@ -48,6 +46,7 @@ namespace WebApplication1.Controllers
                 var getUser = db.CLIENTS.Where(x => x.CLIENT_LOGIN == login && x.CLIENT_PASSWORD == password).First();
                 var films = (from film in db.FILMS select film).ToList();
                 Session["login"] = getUser.CLIENT_LOGIN;
+               
                 return RedirectToAction("Index");
             } catch (Exception e)
             {
@@ -69,6 +68,41 @@ namespace WebApplication1.Controllers
             return View();
         }
 
+        public ActionResult CRUD()
+        {
+            Model1 db = new Model1();
+            SelectList genres = new SelectList(db.GENRES, "GENRE_ID", "NAME_GENRE");
+            ViewBag.Genres = genres;
+
+            SelectList countries = new SelectList(db.COUNTRIES, "COUNTY_ID", "NAME_COUNTRY");
+            ViewBag.Country = countries;
+
+            SelectList studies = new SelectList(db.STUDIES, "STUDIA_ID", "NAME_STUDIA");
+            ViewBag.Studies = studies;
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddFilm(FILMS films)
+        {
+            Model1 db = new Model1();
+            db.Entry(films).State = System.Data.Entity.EntityState.Added;
+            db.SaveChanges();
+            Session["added"] = "Запись добавлена";
+            return RedirectToAction("CRUD");
+        }
+
+        [HttpPost]
+        public ActionResult UpdateFilm(FILMS films)
+        {
+            Model1 db = new Model1();
+            db.Entry(films).State = System.Data.Entity.EntityState.Modified;
+            db.SaveChanges();
+            Session["updated"] = "Запись обнавлена";
+            return RedirectToAction("ShowAllDataFromDb");
+        }
+
         public ActionResult RegistrationUser(string login, string password, string role)
         {
             
@@ -83,6 +117,48 @@ namespace WebApplication1.Controllers
             db.SaveChanges();
 
             return RedirectToAction("Index");
+        }
+
+        public ActionResult ShowAllDataFromDb()
+        {
+            Model1 db = new Model1();
+            var films = (from film in db.FILMS select film).ToList();
+            return View(films);
+        }
+
+        public ActionResult Update(int id)
+        {
+            Session["update_id"] = id;
+            Model1 db = new Model1();
+            string name = db.FILMS.Where(x => x.FILM_ID == id).Select(x => x.NAME_FILM).First();
+            ViewBag.Name = name;
+
+            SelectList countries = new SelectList(db.COUNTRIES, "COUNTY_ID", "NAME_COUNTRY");
+            ViewBag.Country = countries;
+            string country = db.FILMS.Where(x => x.FILM_ID == id).Select(x => x.COUNTRIES.NAME_COUNTRY).First();
+            Session["CountryUpdate"] = country;
+
+            SelectList genres = new SelectList(db.GENRES, "GENRE_ID", "NAME_GENRE");
+            ViewBag.Genres = genres;
+            string genre = db.FILMS.Where(x => x.FILM_ID == id).Select(x => x.GENRES.NAME_GENRE).First();
+            Session["GenreUpdate"] = genre;
+
+            SelectList studies = new SelectList(db.STUDIES, "STUDIA_ID", "NAME_STUDIA");
+            ViewBag.Studies = studies;
+            string studia = db.FILMS.Where(x => x.FILM_ID == id).Select(x => x.STUDIES.NAME_STUDIA).First();
+            Session["StudiaUpdate"] = studia;
+
+
+            var year = db.FILMS.Where(x => x.FILM_ID == id).Select(x => x.YEAR_FILMS).First();
+            ViewBag.Year = year;
+
+            var length = db.FILMS.Where(x => x.FILM_ID == id).Select(x => x.LENGTH_FILM).First();
+            ViewBag.Length = length;
+
+            var rating = db.FILMS.Where(x => x.FILM_ID == id).Select(x => x.RATING).First();
+            ViewBag.Rating = rating;
+
+            return View();
         }
     }
 }
